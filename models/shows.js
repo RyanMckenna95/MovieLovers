7bf81d636c8492fc4731170f3302941dfadf516d
Ryan Mckenna <mckenna1995@gmail.com>
2019-10-22 10:19:09 +0100

    Added shows model

---
+++ models/shows.js
@@ -0,0 +1,18 @@
+const shows = [
+    {id:100000, title:'House', season:'1', released:'2004', cost:24.50, stock:1530},
+    {id:100000, title:'House', season:'2', released:'2005', cost:21.00, stock:1900},
+    {id:100000, title:'House', season:'3', released:'2006', cost:17.50, stock:1290},
+    {id:100000, title:'House', season:'4', released:'2007', cost:13.99, stock:1700},
+    {id:100000, title:'House', season:'5', released:'2008', cost:20.00, stock:3030},
+    {id:100000, title:'House', season:'6', released:'2009', cost:15.30, stock:5700},
+    {id:100000, title:'House', season:'7', released:'2010', cost:28.00, stock:6900},
+    {id:100000, title:'House', season:'8', released:'2011', cost:30.00, stock:8930},
+    {id:100001, title:'The Good Doctor', season:'1', released:'2017', cost:19.00, stock:4223},
+    {id:100001, title:'The Good Doctor', season:'2', released:'2018', cost:22.00, stock:5500},
+    {id:100001, title:'The Good Doctor', season:'3', released:'2019', cost:31.00, stock:6000},
+    {id:100001, title:'Chernobyl', season:'1', released:'2019', cost:35.00, stock:2600},
+    {id:100001, title:'The boys', season:'1', released:'2019', cost:20.00, stock:11000}
+
+];
+
+module.exports = shows;
